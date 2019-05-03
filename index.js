a09fcd93540f57282684b23e9fad699b20cd18f8
iritwc <iritdabush@gmail.com>
2019-05-03 20:30:54 +0300

    Update title

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: 'React Bubble Visualizer',
+      name: 'React  Bubble-Sort  Visualizer',
       list: [5,3,8,0,7,1,9,2,6,4]//[34,1001,2,5,3,9,8,1,5] //,-79,85,24,1,3,45,36,76,22,0,-12]
     };
   }
